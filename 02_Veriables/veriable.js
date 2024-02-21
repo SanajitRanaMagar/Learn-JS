000818b95d26dc9533ebb9e899c2f19a71c6b288
SanajitRanaMagar <160740244+SanajitRanaMagar@users.noreply.github.com>
2024-02-21 13:43:13 +0000

    Container config done

---
+++ 02_Veriables/veriable.js
@@ -0,0 +1,13 @@
+const accountID = 123456
+var accountPassword = "sanajit" // we should not use var because it is an global scoped veriable, it has a great problem in block scope .
+let accountEmail = " sanajit@gmail.com"
+accountCity = "Kathmandu"
+//JS has an for giving nature which means that if error occurs JS will try to make it right by default.
+
+//accountID = 2323232//This is not allowed, constant veriables can't be changed.
+let accounrState;
+accountPassword = "Rana"
+accountEmail="rana@gamil.com"
+accountCity = "Pokhara"
+console.log(accountID)
+console.table([accountID,accountEmail,accountPassword,accountCity,accounrState])
