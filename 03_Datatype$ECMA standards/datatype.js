ad8a45e09e2458dd7a62854e0a84ce1820cc7f99
SanajitRanaMagar <160740244+SanajitRanaMagar@users.noreply.github.com>
2024-02-21 14:21:36 +0000

    Container config done

---
+++ 03_Datatype$ECMA standards/datatype.js
@@ -0,0 +1,11 @@
+console.log('datatype')
+
+
+let a="sanajit" // this is an string
+let b=123 // this is an number // 2 to the root of 53 is the max capacity 
+let c=true //this is an boolen
+let d=undefined //this is an undefined
+let e=null //this is an object
+//these are the premitive datatype. We have other dataype too like symbol(this is use for uniqueness),bigint(this is use to store large number of numbers)
+
+console.table(typeof a,typeof b,typeof c,typeof d,typeof e)
